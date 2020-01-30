e2c5b51d79209f4cad7de952382188609556f7a7
Samir Hinojosa <samirhinojosa@gmail.com>
2020-01-30 17:40:14 +0100

    change the core of the project to dipomas & badges

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Django back-blog
+# Django Open Diplomas
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/c339f23b0d654f458213cca0c34f28a6)](https://app.codacy.com/manual/samirhinojosa/django-back-blog?utm_source=github.com&utm_medium=referral&utm_content=samirhinojosa/django-back-blog&utm_campaign=Badge_Grade_Dashboard)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- djangoopendiplomas/asgi.py
+++ djangoopendiplomas/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangobackblog project.
+ASGI config for djangoopendiplomas project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobackblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoopendiplomas.settings')
 
 application = get_asgi_application()
