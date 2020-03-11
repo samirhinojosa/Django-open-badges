f7926a91bb5fd8f6909755ab261440f4022ac205
Samir Hinojosa <samirhinojosa@gmail.com>
2020-03-11 19:07:30 +0100

    updating .travis.yml in development

--- djangoopendiplomas/config/development.py
+++ djangoopendiplomas/config/development.py
@@ -9,13 +9,22 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+if 'TRAVIS' in os.environ:
+    DB_HOST = 'localhost'
+else:
+    DB_HOST = 'db'
+
+DB_NAME = 'dop'
+DB_USER = 'project'
+DB_PASSWORD = 'project'
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "dop",
-        "USER": "project",
-        "PASSWORD": "project",
-        "HOST": "db",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': f'{DB_USER}',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': f'{DB_HOST}',
+        'PORT': '5432',
     }
 }
