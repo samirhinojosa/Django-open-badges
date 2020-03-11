e822a47b51ef10374874e141f0b58366202a5925
Samir Hinojosa <samirhinojosa@gmail.com>
2020-03-11 11:50:49 +0100

    updating .travis.yml

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoopendiplomas.config.development")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoopendiplomas.config")
     if os.environ.get("DJANGO_SETTINGS_MODULE") == "djangoopendiplomas.config.development":
         from django.core.management.commands.runserver import Command as runserver
         runserver.default_addr = "0.0.0.0"
